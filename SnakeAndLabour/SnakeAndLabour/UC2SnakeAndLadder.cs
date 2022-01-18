fee58e1fb025f220915c722c2d1fe7d0307bae29
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-01-18 15:25:59 +0530

    [Raja]:Added UC2Snake and ladder  program  to implemented in snakeandladder

---
+++ SnakeAndLabour/SnakeAndLabour/UC2SnakeAndLadder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLabour
+{
+    internal class UC2SnakeAndLadder()
+    {
+        private int InitialPosition { get; } = 0;
+    private int CurrentPosition { get; set; }
+
+    private int RollDice()
+    {
+        Random random = new Random();
+        return random.Next(0, 7);
+    }
+
+    public static void Solution()
+    {
+        UC2SnakeAndLadder app = new UC2SnakeAndLadder();
+        app.CurrentPosition = app.InitialPosition;
+        Console.WriteLine($"Current position is at: {app.CurrentPosition}");
+        Console.WriteLine($"Result of rolling dice: {app.RollDice()}");
+
+
+
+
+
+
+    }
+}

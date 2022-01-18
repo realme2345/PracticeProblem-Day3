2e726e16d96c27fb396c893e3ac0475199d7cf7b
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-01-18 15:28:08 +0530

    [Raja]:Added UC3Snake and ladder  program  to implemented in snakeandladder

---
+++ SnakeAndLabour/SnakeAndLabour/UC3SnakeAndLadder.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLabour
+{
+    class UC3SnakeAndLadder
+    {
+        private int InitialPosition { get; } = 0;
+        private int CurrentPosition { get; set; }
+
+        private static int RollDice()
+        {
+            Random random = new Random();
+            return random.Next(1, 7);
+        }
+
+        private static string CheckOption()
+        {
+            Random random = new Random();
+            int option = random.Next(0, 3);
+        Step:
+            switch (option)
+            {
+                case 0:
+                    return "no_play";
+                case 1:
+                    return "ladder";
+                case 2:
+                    return "snake";
+                default:
+                    goto Step;
+            }
+        }
+
+        private static int Play(string option, int position, int roll)
+        {
+            if (option == "ladder") position += roll;
+            else if (option == "snake")
+            {
+                if (position - roll > 0) position -= roll;
+            }
+            else position -= 0;
+            return position;
+        }
+
+        public static void Solution()
+        {
+            UC3SnakeAndLadder app = new UC3SnakeAndLadder();
+            app.CurrentPosition = app.InitialPosition;
+
+        Step:
+            Console.Write("Continue [Y]es/[N]o: ");
+            string choise = Console.ReadLine().ToLower();
+            if (choise == "y")
+            {
+                string option = CheckOption();
+                int roll = RollDice();
+
+                app.CurrentPosition = Play(option, app.CurrentPosition, roll);
+
+                Console.WriteLine($"You got: {option}/{roll} and Position is at: {app.CurrentPosition}");
+
+                goto Step;
+            }
+
+            Console.WriteLine($"Result of rolling dice: {app.CurrentPosition}");
+        }
+    }
+
+}

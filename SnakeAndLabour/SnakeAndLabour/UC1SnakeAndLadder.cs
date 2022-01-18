d8090a59e86873e8cfdc13436f694c1bee7db150
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-01-18 15:21:09 +0530

    [Raja]:Added UC1Snake and ladder  program  to implemented in snakeandladder

---
+++ SnakeAndLabour/SnakeAndLabour/UC1SnakeAndLadder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLabour
+{
+    internal  class UC1SnakeAndLadder
+    {
+        private int InitialPosition { get; } = 0;
+        private int CurrentPosition { get; set; }
+        public static void Solution()
+        {
+            UC1SnakeAndLadder app = new UC1SnakeAndLadder();
+            CurrentPosition = InitialPosition;
+            Console.WriteLine($"current position is at {app.CurrentPosition}");
+        }
+    
+    
+    }
+    
+}
